0837b34fd27d99aebd3812d3c1a5e0090cb92a9a
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-04 02:28:30 +0000

    started coding

--- lib/pokemonscrapper.rb
+++ lib/pokemonscrapper.rb
@@ -1,9 +1,13 @@
 require 'pry'
+require 'open-uri'
+require 'nokogiri'
 
-def hello
-binding.pry
-puts "testing"
-
+class PokemonScraper
+  def self.scrape_pokemon
+    pokemon_url = "https://www.pokemon.com/us/pokedex/"
+    pokemon_web = open(pokemon_url).read 
+    parsed_html = Nokogiri::HTML(pokemon_web)
+    
+  end
+  
 end
-
-hello
