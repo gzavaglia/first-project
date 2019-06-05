89e9c84aca7c00a34d481a6742e68508ef3056d8
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-05 02:58:43 +0000

    filtered names and links

--- lib/pokemonscrapper.rb
+++ lib/pokemonscrapper.rb
@@ -4,12 +4,19 @@ require 'nokogiri'
 
 class PokemonScraper
   def self.scrape_pokemon
-    pokemon_url = "https://www.pokemon.com/us/pokedex/"
-    pokemon_web = open(pokemon_url).read 
-    parsed_html = Nokogiri::HTML(pokemon_web)
-    binding.pry 
-  end
-  
+    #harvest
+    poke_url = "https://rankedboost.com/pokemon-go/pokedex/"
+    poke_html = open(poke_url).read 
+    
+    #filter
+    parsed_pokemon = Nokogiri::HTML(poke_html)
+    #here I get all the names 
+    pokemon_names = parsed_pokemon.css("span.PokemonName").map(&:text)
+    #this gives me all the links 
+    parsed_pokemon.css("span.pokemontypeImgs a").collect do |pok_link|
+      pokemon_links = pok_link.attribute('href').value
+    end
+  end #end method 
 end
 
 PokemonScraper.scrape_pokemon
