dfa9626a4a65049735e9e3b0d2bb7369486108ba
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-06 22:36:28 +0000

    The Environment was created

---
+++ lib/environment.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/pokemonscraper.rb"
+require_relative "../lib/pokemon.rb"
+require_relative "../lib/play.rb"
+require 'nokogiri'
+require 'open-uri'
+
+module Pokemons
+  class Error < StandardError
+  #####
+  end
+end
